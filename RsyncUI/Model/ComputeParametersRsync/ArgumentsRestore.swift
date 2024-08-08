4f62692783d24ab6c7769fa4b30e58257d7c7980
Thomas Evensen <thomeven@gmail.com>
2024-08-08 16:27:44 +0200

    update

--- RsyncUI/Model/ComputeParametersRsync/ArgumentsRestore.swift
+++ RsyncUI/Model/ComputeParametersRsync/ArgumentsRestore.swift
@@ -5,51 +5,93 @@
 //  Created by Thomas Evensen on 13/10/2019.
 //  Copyright © 2019 Thomas Evensen. All rights reserved.
 //
+// swiftlint:disable line_length
 
 import Foundation
+import RsyncArguments
 
-final class ArgumentsRestore: ComputeRsyncParameters {
+@MainActor
+final class ArgumentsRestore {
     var config: SynchronizeConfiguration?
     var restoresnapshotbyfiles: Bool = false
 
-    func argumentsrestore(dryRun: Bool, forDisplay: Bool, tmprestore: Bool) -> [String]? {
+    func argumentsrestore(dryRun _: Bool, forDisplay: Bool, tmprestore: Bool) -> [String]? {
         if let config {
-            // Restore arguments
-            localCatalog = config.localCatalog
-            let snapshot: Bool = (config.snapshotnum != nil) ? true : false
-            if snapshot {
-                if restoresnapshotbyfiles == true {
-                    // This is a hack for fixing correct restore for files
-                    // from a snapshot. The last snapshot is base for restore
-                    // of files. The correct snapshot is added within the
-                    // ObserveableRestore which is used within the RestoreView
-                    // --archive --verbose --compress --delete -e "ssh -i ~/.ssh_rsyncosx/rsyncosx -p 22"
-                    // --exclude-from=/Users/thomas/Documents/excludersync/exclude-list-github.txt --dry-run --stats
-                    // thomas@backup:/backups/snapshots/Github/85/AlertToast /Users/thomas/tmp
-                    remoteargs(config: config)
-                } else {
-                    // --archive --verbose --compress --delete -e "ssh -i ~/.ssh_rsyncosx/rsyncosx -p 22"
-                    // --exclude-from=/Users/thomas/Documents/excludersync/exclude-list-github.txt --dry-run --stats
-                    // thomas@backup:/backups/snapshots/Github/85/ /Users/thomas/tmp
-                    remoteargssnapshot(config: config)
-                }
-            } else {
-                // --archive --verbose --compress --delete -e "ssh -i ~/.ssh_rsyncosx/rsyncosx -p 22" --backup
-                // --backup-dir=../backup_Documents --dry-run --stats thomas@backup:/backups/Documents/
-                // Users/thomas/tmp
-                remoteargs(config: config)
-            }
-            setParameters1To6(config: config, forDisplay: forDisplay, verify: false)
-            setParameters8To14(config: config, dryRun: dryRun, forDisplay: forDisplay)
-            argumentsforrestore(forDisplay: forDisplay, tmprestore: tmprestore)
-            return arguments
+            let rsyncparametersrestore = RsyncParametersRestore(task: config.task,
+                                                                parameter1: config.parameter1,
+                                                                parameter2: config.parameter2,
+                                                                parameter3: config.parameter3,
+                                                                parameter4: config.parameter4,
+                                                                parameter5: config.parameter5,
+                                                                parameter6: config.parameter5,
+                                                                parameter8: config.parameter8,
+                                                                parameter9: config.parameter9,
+                                                                parameter10: config.parameter10,
+                                                                parameter11: config.parameter11,
+                                                                parameter12: config.parameter12,
+                                                                parameter13: config.parameter13,
+                                                                parameter14: config.parameter14,
+                                                                sshport: String(config.sshport ?? -1),
+                                                                sshkeypathandidentityfile: config.sshkeypathandidentityfile ?? "",
+                                                                sharedsshport: String(SharedReference.shared.sshport ?? -1),
+                                                                sharedsshkeypathandidentityfile: SharedReference.shared.sshkeypathandidentityfile,
+                                                                localCatalog: config.localCatalog,
+                                                                offsiteCatalog: config.offsiteCatalog,
+                                                                offsiteServer: config.offsiteServer,
+                                                                offsiteUsername: config.offsiteUsername,
+                                                                sharedpathforrestore: SharedReference.shared.pathforrestore ?? "",
+                                                                snapshotnum: config.snapshotnum ?? -1,
+                                                                rsyncdaemon: config.rsyncdaemon ?? -1)
+            // rsyncparametersrestore.argumentsrestore(forDisplay: forDisplay, verify: false, dryrun: dryRun, restoresnapshotbyfiles: restoresnapshotbyfiles, tmprestore: tmprestore)
+            rsyncparametersrestore.argumentsrestore(forDisplay: forDisplay, verify: false, dryrun: true, restoresnapshotbyfiles: restoresnapshotbyfiles, tmprestore: tmprestore)
+            return rsyncparametersrestore.computedarguments
         }
+
         return nil
     }
 
+    /*
+       func argumentsrestore(dryRun: Bool, forDisplay: Bool, tmprestore: Bool) -> [String]? {
+           if let config {
+               // Restore arguments
+               localCatalog = config.localCatalog
+               let snapshot: Bool = (config.snapshotnum != nil) ? true : false
+               if snapshot {
+                   if restoresnapshotbyfiles == true {
+                       // This is a hack for fixing correct restore for files
+                       // from a snapshot. The last snapshot is base for restore
+                       // of files. The correct snapshot is added within the
+                       // ObserveableRestore which is used within the RestoreView
+                       // --archive --verbose --compress --delete -e "ssh -i ~/.ssh_rsyncosx/rsyncosx -p 22"
+                       // --exclude-from=/Users/thomas/Documents/excludersync/exclude-list-github.txt --dry-run --stats
+                       // thomas@backup:/backups/snapshots/Github/85/AlertToast /Users/thomas/tmp
+                       remoteargs(config: config)
+                   } else {
+                       // --archive --verbose --compress --delete -e "ssh -i ~/.ssh_rsyncosx/rsyncosx -p 22"
+                       // --exclude-from=/Users/thomas/Documents/excludersync/exclude-list-github.txt --dry-run --stats
+                       // thomas@backup:/backups/snapshots/Github/85/ /Users/thomas/tmp
+                       remoteargssnapshot(config: config)
+                   }
+               } else {
+                   // --archive --verbose --compress --delete -e "ssh -i ~/.ssh_rsyncosx/rsyncosx -p 22" --backup
+                   // --backup-dir=../backup_Documents --dry-run --stats thomas@backup:/backups/Documents/
+                   // Users/thomas/tmp
+                   remoteargs(config: config)
+               }
+               setParameters1To6(config: config, forDisplay: forDisplay, verify: false)
+               setParameters8To14(config: config, dryRun: dryRun, forDisplay: forDisplay)
+               argumentsforrestore(forDisplay: forDisplay, tmprestore: tmprestore)
+               return arguments
+           }
+           return nil
+
+       }
+     */
+
     init(config: SynchronizeConfiguration?, restoresnapshotbyfiles: Bool) {
-        super.init()
         self.config = config
         self.restoresnapshotbyfiles = restoresnapshotbyfiles
     }
 }
+
+// swiftlint:enable line_length
