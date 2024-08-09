d271374e07eeb65fc69fd0909a510865e001675b
Thomas Evensen <thomeven@gmail.com>
2024-08-09 12:52:14 +0200

    Update ArgumentsVerify.swift

--- RsyncUI/Model/ComputeParametersRsync/ArgumentsVerify.swift
+++ RsyncUI/Model/ComputeParametersRsync/ArgumentsVerify.swift
@@ -5,36 +5,61 @@
 //  Created by Thomas Evensen on 13/10/2019.
 //  Copyright © 2019 Thomas Evensen. All rights reserved.
 //
+// swiftlint:disable line_length
 
 import Foundation
+import RsyncArguments
 
-final class ArgumentsVerify: ComputeRsyncParameters {
+@MainActor
+final class ArgumentsVerify {
     var config: SynchronizeConfiguration?
 
     func argumentsverify(forDisplay: Bool) -> [String]? {
         if let config {
-            localCatalog = config.localCatalog
-            remoteargs(config: config)
-            setParameters1To6(config: config, forDisplay: forDisplay, verify: true)
-            setParameters8To14(config: config, dryRun: true, forDisplay: forDisplay)
+            let rsyncparameterssynchronize = RsyncParametersSynchronize(task: config.task,
+                                                                        parameter1: config.parameter1,
+                                                                        parameter2: config.parameter2,
+                                                                        parameter3: config.parameter3,
+                                                                        parameter4: config.parameter4,
+                                                                        parameter5: config.parameter5,
+                                                                        parameter6: config.parameter5,
+                                                                        parameter8: config.parameter8,
+                                                                        parameter9: config.parameter9,
+                                                                        parameter10: config.parameter10,
+                                                                        parameter11: config.parameter11,
+                                                                        parameter12: config.parameter12,
+                                                                        parameter13: config.parameter13,
+                                                                        parameter14: config.parameter14,
+                                                                        sshport: String(config.sshport ?? -1),
+                                                                        sshkeypathandidentityfile: config.sshkeypathandidentityfile ?? "",
+                                                                        sharedsshport: String(SharedReference.shared.sshport ?? -1),
+                                                                        sharedsshkeypathandidentityfile: SharedReference.shared.sshkeypathandidentityfile,
+                                                                        localCatalog: config.localCatalog,
+                                                                        offsiteCatalog: config.offsiteCatalog,
+                                                                        offsiteServer: config.offsiteServer,
+                                                                        offsiteUsername: config.offsiteUsername,
+                                                                        sharedpathforrestore: SharedReference.shared.pathforrestore ?? "",
+                                                                        snapshotnum: config.snapshotnum ?? -1,
+                                                                        rsyncdaemon: config.rsyncdaemon ?? -1,
+                                                                        rsyncversion3: SharedReference.shared.rsyncversion3)
             switch config.task {
             case SharedReference.shared.synchronize:
-                argumentsforsynchronize(forDisplay: forDisplay)
+                rsyncparameterssynchronize.argumentsforsynchronize(forDisplay: forDisplay, verify: true, dryrun: true)
             case SharedReference.shared.snapshot:
-                linkdestparameter(config: config, verify: true)
-                argumentsforsynchronizesnapshot(forDisplay: forDisplay)
+                rsyncparameterssynchronize.argumentsforsynchronizesnapshot(forDisplay: forDisplay, verify: true, dryrun: true)
             case SharedReference.shared.syncremote:
-                return []
+                rsyncparameterssynchronize.argumentsforsynchronizeremote(forDisplay: forDisplay, verify: true, dryrun: true)
             default:
                 break
             }
-            return arguments
+            return rsyncparameterssynchronize.computedarguments
         }
         return nil
     }
 
     init(config: SynchronizeConfiguration?) {
-        super.init()
         self.config = config
     }
 }
+
+// swiftlint:enable line_length
