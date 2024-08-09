b46039fd84998fac5a96b788d60ff86f1487b325
Thomas Evensen <thomeven@gmail.com>
2024-08-09 13:06:32 +0200

    Update

--- RsyncUI/Model/ComputeParametersRsync/Argumentsforrsync.swift
+++ RsyncUI/Model/ComputeParametersRsync/Argumentsforrsync.swift
@@ -10,19 +10,15 @@ import Foundation
 @MainActor
 struct Argumentsforrsync {
     // Function return arguments for rsync, either arguments for
-    // real runn or arguments for --dry-run for Configuration at selected index
+    // real run or arguments for --dry-run for Configuration at selected index
     func argumentsforrsync(config: SynchronizeConfiguration, argtype: ArgumentsRsync) -> [String] {
         switch argtype {
         case .arg:
-            return ArgumentsSynchronize(config: config).argumentssynchronize(dryRun: false,
-                                                                             forDisplay: false) ?? []
+            ArgumentsSynchronize(config: config).argumentssynchronize(dryRun: false,
+                                                                      forDisplay: false) ?? []
         case .argdryRun:
-            return ArgumentsSynchronize(config: config).argumentssynchronize(dryRun: true,
-                                                                             forDisplay: false) ?? []
-        case .argdryRunlocalcataloginfo:
-            guard config.task != SharedReference.shared.syncremote else { return [] }
-            return ArgumentsLocalcatalogInfo(config: config).argumentslocalcataloginfo(dryRun: true,
-                                                                                       forDisplay: false) ?? []
+            ArgumentsSynchronize(config: config).argumentssynchronize(dryRun: true,
+                                                                      forDisplay: false) ?? []
         }
     }
 }
@@ -31,5 +27,4 @@ struct Argumentsforrsync {
 enum ArgumentsRsync {
     case arg
     case argdryRun
-    case argdryRunlocalcataloginfo
 }
